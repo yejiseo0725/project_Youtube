a076742ffb7480aa6197275cb189310d22d45960
YEJI SEO <yejiseo0725@gmail.com>
2023-12-20 21:45:12 +0900

    my first server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+app.get()
+
+const handleListening = () => console.log(`Listening on port http://localhost:${PORT}🧀`);
+
+app.listen(4000, handleListening);
+
