7074872dba1a339f80f3e8374f2e4d5c7381cd4e
YEJI SEO <yejiseo0725@gmail.com>
2023-12-29 22:31:23 +0900

    middleware 2

--- src/server.js
+++ src/server.js
@@ -4,20 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 }
-
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>허용되지 않아요. Not Allowed!</h1>")
+  }
+  console.log("You may continue. 당신은 통과에요! ^_^");
+  next();
+}
 const handleHome = (req, res) => {
   return res.send("I love you :D");
 };
-// const handleLogin = (req, res) => {
-//   return res.send({ message: "Login here!!!" });
-// };
 
-app.get("/", gossipMiddleware, handleHome);
-// app.get("/login", handleLogin);
+const handledProtected = (req, res) => {
+  return res.send("Welcome to the private lounge...:3");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handledProtected);
 
 const handleListening = () => console.log(`Listening on port http://localhost:${PORT}🧀`);
 
