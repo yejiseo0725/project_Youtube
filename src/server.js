5b3884e25e8e23589189b99e461103992ec455ff
YEJI SEO <yejiseo0725@gmail.com>
2024-01-02 16:56:42 +0900

    middleware recap

--- src/server.js
+++ src/server.js
@@ -4,12 +4,28 @@ const PORT = 4000;
 
 const app = express();
 
+const routerLogger = (req, res, next) => {
+  console.log("PATH: ", req.path);
+  next();
+}
+
+const methodLogger = (req, res, next) => {
+  console.log("METHOD: ", req.method);
+  next();
+}
+
 const home = (req, res) => {
+  console.log("I will response,,,");
   return res.send("<h1>HELLO</h1>");
 }
 
+const login = (req, res) => {
+  return res.send("Login!");
+}
+
+app.use(methodLogger, routerLogger);
 app.get("/", home);
-// app.get("/", (req, res) => res.send("hello"));
+app.get("/login", login);
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
