29cd2064c3363f329b4e10f4d01146ac440e81c0
YEJI SEO <yejiseo0725@gmail.com>
2024-01-03 17:35:00 +0900

    experts, separate Routers and Controllers

--- src/server.js
+++ src/server.js
@@ -1,23 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  console.log("I will response,,,");
-  return res.send("<h1>HELLO</h1>");
-}
-
-const login = (req, res) => {
-  return res.send("Login!");
-}
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
