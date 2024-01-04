d372c4b2f569f10f7fd2de82a7c8d807b49b7a31
YEJI SEO <yejiseo0725@gmail.com>
2024-01-04 21:35:02 +0900

    URL parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,17 @@
 export const trending = (req, res) => res.send("Home Page Trending Videos");
-export const see = (req, res) => res.send("See");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+    return res.send(`Watch Video: #${req.params.id}`);
+};
+export const edit = (req, res) => {
+    console.log(req.params);
+    return res.send("Edit");
+};
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    return res.send("Delete Video");
+}
 export const search = (req, res) => res.send("Search!");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+
+
+
