621c71f980461acf2a578a859f5675decd4eb680
YEJI SEO <yejiseo0725@gmail.com>
2024-01-11 16:27:02 +0900

    URL: absolute and relative

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,37 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 2,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 3,
-        },
-    ];
     return res.render("home", { pageTitle: "Home", videos })
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id - 1];
-    return res.render("watch", {pageTitle: `Watching ${video .title}`});
+    return res.render("watch", {pageTitle: `Watching ${video .title}`, video});
 };
 export const edit = (req, res) => res.render("edit");
 export const deleteVideo = (req, res) => {
