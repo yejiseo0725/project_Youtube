f52697b55f1755f6f49730d348a08fbfc12b7d9f
YEJI SEO <yejiseo0725@gmail.com>
2024-01-25 20:31:34 +0900

    POST

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,11 +33,14 @@ export const watch = (req, res) => {
     const video = videos[id - 1];
     return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => {
+export const getEdit = (req, res) => {
     const { id } = req.params;
     const video = videos[id - 1];
     return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
+
+// export const postEdit = (req, res) => {};
+
 export const deleteVideo = (req, res) => {
     console.log(req.params);
     return res.send("Delete Video");
