b2ffae9109db8c713686b4e2d5890298298bb2bc
YEJI SEO <yejiseo0725@gmail.com>
2024-01-29 22:01:57 +0900

    practice of Upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -46,6 +46,24 @@ export const postEdit = (req, res) => {
     return res.redirect(`/videos/${id}`);
 };
 
+export const getUproad = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+    const { title } = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "Just Now",
+        views: 0,
+        id: videos.length + 1,
+    }
+    videos.push(newVideo);
+    return res.redirect("/");
+};
+
 
 
 
